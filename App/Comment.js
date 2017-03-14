3775f427b2c1972c82601744cdc494e3b98c4e04
John Walley <john@Walley.org.uk>
2017-03-14 23:15:56 +0000

    Add Hyperlinks

--- App/Comment.js
+++ App/Comment.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import {
+  Linking,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
+import Hyperlink from 'react-native-hyperlink'
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
+  info: {
+    fontWeight: 'bold', color: '#888888'
+  }
 });
 
 export default Comment = ({ level, content, user, timeAgo }) =>
   <View style={{ paddingLeft: level * 10 + 10 }}>
-    <Text style={{ fontWeight: 'bold', color: '#888888' }}>{user}, {timeAgo}</Text>
-    <Text>{content}</Text>
+    <Text style={styles.info}>{user}, {timeAgo}</Text>
+    <Hyperlink onPress={url => Linking.openURL(url).catch(err => console.error('An error occurred', err))} linkStyle={{ color: '#2980b9' }}>
+      <Text>{content}</Text>
+    </Hyperlink>
   </View>
