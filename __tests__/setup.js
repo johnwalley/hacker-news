92cb7e1b5a32d32f8b6d639e5c69926219063fe1
johnwalley <john@walley.org.uk>
2017-04-01 10:31:47 +0100

    Tests now pass

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -4,14 +4,15 @@ jest.mock('Linking', () => {
     removeEventListener: jest.fn(),
     openURL: jest.fn(),
     canOpenURL: jest.fn(),
-    getInitialURL: jest.fn(),
+    getInitialURL: jest.fn(() => new Promise((resolve, reject) => {
+      resolve(null)
+    })),
   }
 });
 
 
-require('isomorphic-fetch');
+fetch = jest.fn((url, options) => new Promise((resolve, reject) => {
+  resolve({ status: 201, json: () => (response) })
+}));
+
 require('es6-promise').polyfill();
-
-require('react-native-mock/mock');
-
-global.fetch = require('jest-fetch-mock');
